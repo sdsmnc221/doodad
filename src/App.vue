0297e9175b2ce86bb6ef94435b9b59eb9d9ce66f
An <antr.2201@gmail.com>
2019-10-29 11:48:58 +0100

    first

---
+++ src/App.vue
@@ -0,0 +1,42 @@
+
+<template>
+  <div class="app">
+    <nav>Hello</nav>
+    <div></div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  data: function() {
+    return {
+      name: "Hello World!"
+    };
+  }
+});
+</script>
+
+<style lang="scss">
+@import "./styles";
+
+body {
+  width: 100vw;
+  height: 100vh;
+  overflow: hidden;
+}
+
+.app {
+  width: 100%;
+  height: 100%;
+  overflow: hidden;
+  position: relative;
+  display: flex;
+
+  nav {
+    background-color: black;
+    flex-basis: 40%;
+  }
+}
+</style>

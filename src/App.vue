bce8119a87a514ae4b3aa1e3ddd05c13f58a9cd5
An <antr.2201@gmail.com>
2019-10-31 15:51:01 +0100

    add event bus & resize event

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,49 @@
 
 <template>
-	<div class="app">
-		<nav>
-			<router-link to="/">hi-</router-link>
-			<router-link to="/hello-world">hello world-</router-link>
-		</nav>
-		<main>
-			<router-view />
-		</main>
-	</div>
+  <div class="app">
+    <nav>
+      <router-link to="/">hi-</router-link>
+      <router-link to="/hello-world">hello world-</router-link>
+    </nav>
+    <main>
+      <router-view/>
+    </main>
+  </div>
 </template>
 
+<script>
+import EventBus from './classes/EventBus';
+
+export default {
+	name: 'App',
+	mounted() {
+		window.addEventListener('resize', () => EventBus.$emit('resize'));
+	}
+};
+</script>
+
+
 <style lang="scss">
-	@import './styles';
+@import './styles';
 
-	.app {
-		display: flex;
+.app {
+	display: flex;
 
-		nav {
-			background-color: black;
-			color: white;
-			flex-basis: 40%;
+	nav {
+		background-color: black;
+		color: white;
+		flex-basis: 40%;
 
-			a {
-				display: block;
-			}
-		}
-
-		main {
-			overflow: hidden;
-			color: black;
-			flex: 1;
-			padding: 64px;
+		a {
+			display: block;
 		}
 	}
+
+	main {
+		overflow: hidden;
+		color: black;
+		flex: 1;
+		padding: 64px;
+	}
+}
 </style>
--- src/views/HelloWorld.vue
+++ src/views/HelloWorld.vue
@@ -1,46 +1,57 @@
 <template>
-	<div class="wrapper">
-		<css-doodle ref="doodle">
-			:doodle {
-			@grid: 10 / 100%;
-			}
-			background: #60569e;
-			transform: scale(@rand(.2, .9));
-		</css-doodle>
-	</div>
+  <div class="wrapper" @resize="resize">
+    <css-doodle ref="doodle">
+      :doodle {
+      @grid: 10 / 100%;
+      }
+      background: #60569e;
+      transform: scale(@rand(.2, .9));
+    </css-doodle>
+  </div>
 </template>
 
 <script>
-	import 'css-doodle';
+import 'css-doodle';
+import EventBus from '../classes/EventBus';
 
-	export default {
-		name: 'HelloWorld',
-		data() {
-			return {
-				bounding: null
-			};
+export default {
+	name: 'HelloWorld',
+	data() {
+		return {
+			bounding: null
+		};
+	},
+	mounted() {
+		setTimeout(() => {
+			this.size();
+		}, 1000);
+
+		EventBus.$on('resize', this.resize);
+	},
+	methods: {
+		size() {
+			this.bounding = this.$el.getBoundingClientRect();
+
+			const { width, height } = this.bounding;
+			const doodle = this.$refs['doodle'];
+
+			if (width > height) {
+				doodle.style.width = `${height}px`;
+			} else {
+				doodle.style.height = `${width}px`;
+			}
 		},
-		mounted() {
-			setTimeout(() => {
-				this.bounding = this.$el.getBoundingClientRect();
-
-				const { width, height } = this.bounding;
-				const doodle = this.$refs['doodle'];
-
-				if (width > height) {
-					doodle.style.width = `${height}px`;
-				} else {
-					doodle.style.height = `${width}px`;
-				}
-			}, 1000);
+		resize() {
+			this.size();
 		}
-	};
+	}
+};
 </script>
 
 <style lang="scss" scoped>
-	@import '../styles/theme';
+@import '../styles/theme';
 
-	css-doodle {
-		@include abs-center();
-	}
+css-doodle {
+	@include abs-center();
+}
 </style>

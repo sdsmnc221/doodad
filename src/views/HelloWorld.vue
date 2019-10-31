27f6f1962500d22a17529a79961909a5f1e15817
An <antr.2201@gmail.com>
2019-10-31 18:28:42 +0100

    hello world 01: art piece

--- src/views/HelloWorld.vue
+++ src/views/HelloWorld.vue
@@ -1,18 +1,15 @@
 <template>
   <div class="wrapper" @resize="resize">
-    <css-doodle ref="doodle">
-      :doodle {
-      @grid: 10 / 100%;
-      }
-      background: #60569e;
-      transform: scale(@rand(.2, .9));
-    </css-doodle>
+    <css-doodle ref="doodle" use="var(--doodle)"></css-doodle>
+    <img :src="error" alt>
   </div>
 </template>
 
 <script>
 import 'css-doodle';
 import EventBus from '../classes/EventBus';
+
+import Error from '../assets/error.png';
 
 export default {
 	name: 'HelloWorld',
@@ -21,12 +18,18 @@ export default {
 			bounding: null
 		};
 	},
+	computed: {
+		error() {
+			return Error;
+		}
+	},
+	created() {
+		EventBus.$on('resize', this.resize.bind(this));
+	},
 	mounted() {
 		setTimeout(() => {
 			this.size();
 		}, 1000);
-
-		EventBus.$on('resize', this.resize);
 	},
 	methods: {
 		size() {
@@ -35,14 +38,18 @@ export default {
 			const { width, height } = this.bounding;
 			const doodle = this.$refs['doodle'];
 
-			if (width > height) {
-				doodle.style.width = `${height}px`;
-			} else {
-				doodle.style.height = `${width}px`;
+			if (doodle) {
+				if (width > height) {
+					doodle.style.width = `${height}px`;
+				} else {
+					doodle.style.height = `${width}px`;
+				}
 			}
 		},
 		resize() {
-			this.size();
+			setTimeout(() => {
+				this.size();
+			}, 1000);
 		}
 	}
 };
@@ -51,7 +58,59 @@ export default {
 <style lang="scss" scoped>
 @import '../styles/theme';
 
-css-doodle {
-	@include abs-center();
+.wrapper {
+	img {
+		@include abs-center();
+		z-index: 9;
+		box-shadow: 4px 4px 10px 0 rgba(0, 0, 0, 0.4);
+	}
+
+	css-doodle {
+		@include abs-center();
+	}
+
+	// prettier-ignore
+	--doodle: (
+		:doodle {
+			@grid: 20 / 100%; 
+			grid-gap: 2px;
+		} 
+
+		position: relative;
+		transform: scale(1) translate(0) rotate(@rand(360deg));
+
+		:after {
+			/* content: 'f*ck';
+			font-size: @rand(0.6rem, 1.8rem);
+			font-weight: @pick(100, 300, 500, 900); */
+			content: '';
+			width: @rand(60%, 100%);
+			height: @rand(60%, 100%);
+			background-image: url('../assets/cursor.png');
+			background-size: contain;
+			background-position: center;
+			background-repeat: no-repeat;
+			position: absolute;
+			top: 50%;
+			left: 50%;
+			transform: translate(-50%, -50%);
+			display: block;
+		}
+
+		animation-name: fly;
+		animation-direction: reverse;
+		animation-iteration-count: infinite;
+		animation-fill-mode: none;
+		animation-timing-function: cubic-bezier(0.645, 0.045, 0.355, 1);
+		animation-duration: @rand(12s);
+		animation-delay: @rand(2s);
+
+		@keyframes fly {
+			to {
+				transform: scale(0) translate(calc(@rand(-100%, 100%) * @index()), calc(@rand(-100%, 100%) * @index())) rotate(@rand(360deg));
+				opacity: 0;
+			}
+		}
+	);
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,18 @@
 <template>
-	<div class="wrapper">
-		<h1>
-			this is
-			<strong>doodad</strong>.
-		</h1>
-		<p>
-			created to explore unfold possibilities of
-			<strong>< css-doodle /></strong>.
-		</p>
-	</div>
+  <div class="wrapper">
+    <h1>
+      this is
+      <strong>doodad</strong>.
+    </h1>
+    <p>
+      created to explore unfold possibilities of
+      <strong>< css-doodle /></strong>.
+    </p>
+  </div>
 </template>
 
 <script>
-	export default {
-		name: 'Home'
-	};
+export default {
+	name: 'Home'
+};
 </script>

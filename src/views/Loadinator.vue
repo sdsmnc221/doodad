e44d56cbbbc9b96c43b076a976276d632d981fb5
An <antr.2201@gmail.com>
2019-11-27 18:09:11 +0100

    make a grid

--- src/views/Loadinator.vue
+++ src/views/Loadinator.vue
@@ -1,6 +1,6 @@
 <template>
-	<div class="wrapper" @resize="resize">
-		<css-doodle ref="doodle" use="var(--doodle)"></css-doodle>
+	<div class="wrapper" ref="wrapper" @resize="resize">
+		<css-doodle use="var(--doodle)"></css-doodle>
 	</div>
 </template>
 
@@ -30,7 +30,7 @@
 		mounted() {
 			tick(() => {
 				this.size();
-				this.refresh();
+				// this.refresh();
 			}, 1000);
 		},
 		methods: {
@@ -38,13 +38,13 @@
 				this.bounding = this.$el.getBoundingClientRect();
 
 				const { width, height } = this.bounding;
-				const doodle = this.$refs['doodle'];
+				const wrapper = this.$refs['wrapper'];
 
-				if (doodle) {
+				if (wrapper) {
 					if (width > height) {
-						doodle.style.width = `${height}px`;
+						wrapper.style.width = `${height}px`;
 					} else {
-						doodle.style.height = `${width}px`;
+						wrapper.style.height = `${width}px`;
 					}
 				}
 			},
@@ -86,80 +86,83 @@
 	@import '../styles/theme';
 
 	.wrapper {
-		img {
-			@include abs-center();
-			z-index: 9;
-			box-shadow: 4px 4px 10px 0 rgba(0, 0, 0, 0.4);
-		}
+		@include abs-center();
+		padding: 64px;
+		display: grid;
+		grid-template-columns: repeat(5, 1fr);
+		grid-template-rows: repeat(5, 1fr);
+		justify-items: stretch;
+		align-items: stretch;
 
 		css-doodle {
-			@include abs-center();
+			grid-area: 1 / 1 / span 1 / span 1;
 			--color: $text;
+			background-color: black;
 		}
 
 		--color: black;
 
 		/* prettier-ignore */
 		--doodle: (
-															:doodle {
-																@grid: 3 / 100%; 
-																grid-gap: 72px;
-																
-															} 
+			:doodle {
+				@grid: 3 / 20px; 
+				grid-gap: 72px;
+				
+			} 
 
-															position: relative;
-															z-index: 1;
-															background-color: var(--color);
-															border-radius: 100%;
-															opacity: 1;
-															filter: blur(0) brightness(1);
-															
-															animation-name: fade;
-															animation-direction: alternate;
-															animation-iteration-count: infinite;
-															animation-fill-mode: both;
-															animation-timing-function: #{$ease-in-out-quart};
-															animation-duration: @r(0.4, 0.8)s;
-															
-															:after {
-																content: '';
-																display: block;
-																position: absolute;
-																z-index: -1;
-																width: 100%;
-																height: 100%;
-																top: 50%;
-																left: 50%;
-																transform: translate(-50%, -50%) scale(0);
-																background-color: var(--color);
-																border-radius: 100%;
-																opacity: 0;
-																filter: blur(0) brightness(1);
-																
-																// animation-name: fade-after;
-																// animation-direction: alternate;
-																// animation-iteration-count: infinite;
-																// animation-fill-mode: both;
-																// animation-timing-function: #{$ease-in-out-quart};
-																// animation-duration: @r(0.4, 0.8)s;
-															}
-															
-															@keyframes fade {
-																to {
-																	opacity: 0.4;
-																	// filter: blur(0.8px) brightness(0.8);
-																}
-															}
+			position: relative;
+			z-index: 1;
+			background-color: var(--color);
+			border-radius: 100%;
+			opacity: 1;
+			filter: blur(0) brightness(1);
+			
+			animation-name: fade;
+			animation-direction: alternate;
+			animation-iteration-count: infinite;
+			animation-fill-mode: both;
+			animation-timing-function: #{$ease-in-out-quart};
+			animation-duration: @r(0.4, 0.8)s;
+			
+			:after {
+				content: '';
+				display: block;
+				position: absolute;
+				z-index: -1;
+				width: 100%;
+				height: 100%;
+				top: 50%;
+				left: 50%;
+				transform: translate(-50%, -50%) scale(0);
+				background-color: var(--color);
+				border-radius: 100%;
+				opacity: 0;
+				filter: blur(0) brightness(1);
+				
+				// animation-name: fade-after;
+				// animation-direction: alternate;
+				// animation-iteration-count: infinite;
+				// animation-fill-mode: both;
+				// animation-timing-function: #{$ease-in-out-quart};
+				// animation-duration: @r(0.4, 0.8)s;
+			}
+			
+			@keyframes fade {
+				to {
+					opacity: 0.4;
+					// filter: blur(0.8px) brightness(0.8);
+				}
+			}
 
-															@keyframes fade-after {
-																to {
-																	transform: translate(-50%, -50%) scale(@r(1.2, 1.6));
-																	opacity: 0.4;
-																	filter: blur(1.2px) brightness(1.2);
-																}
-															}
-															
-														)
+			@keyframes fade-after {
+				to {
+					transform: translate(-50%, -50%) scale(@r(1.2, 1.6));
+					opacity: 0.4;
+					filter: blur(1.2px) brightness(1.2);
+				}
+			}
+			
+		)
 		/* prettier-ignore */
 	}
 </style>
